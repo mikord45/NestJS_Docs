56853c524f404a66f95a84988803a672e07c465e
mikord45 <51155742+mikord45@users.noreply.github.com>
2023-12-15 22:32:33 +0100

    docs - finished Overview-Middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common'
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
 // import { AppController } from './sampleApp/app.controller'
 // import { AppService } from './sampleApp/app.service'
 import { CatsModule } from './cats/cats.module'
+import { CatsController } from './cats/cats.controller'
+import { logger /*, logger2*/ } from './logger.middleware'
 // import { SampleAppModule } from './sampleApp/sample.app.module'
 
 @Module({
@@ -9,4 +16,21 @@ import { CatsModule } from './cats/cats.module'
   // controllers: [AppController],
   // providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(/*logger2,*/ logger)
+      /*.exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST },
+        'cats/(.*)',
+      )*/
+      .forRoutes(
+        CatsController,
+        // {
+        //   path: 'cats',
+        //   method: RequestMethod.GET,
+        // },
+      )
+  }
+}
